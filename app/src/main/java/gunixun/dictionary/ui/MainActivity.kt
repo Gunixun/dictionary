877f01c837dc42ab11bb3cdca60e01637edaa9e9
GanievAR <ganievar@bnipi.rosneft.ru>
2022-06-05 08:44:46 +0500

    animated transitions between screens

--- app/src/main/java/gunixun/dictionary/ui/MainActivity.kt
+++ app/src/main/java/gunixun/dictionary/ui/MainActivity.kt
@@ -2,11 +2,30 @@ package gunixun.dictionary.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import gunixun.dictionary.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun navigationTo(fragment: Fragment, withTransaction: Boolean) {
+        val transaction = supportFragmentManager
+            .beginTransaction()
+            .setCustomAnimations(
+                R.anim.slide_in,
+                R.anim.push_up_out,
+                R.anim.push_up_in,
+                R.anim.slide_out
+            )
+            .replace(R.id.container, fragment)
+
+        if (withTransaction) {
+            transaction.addToBackStack("Transaction")
+        }
+
+        transaction.commit()
     }
 }

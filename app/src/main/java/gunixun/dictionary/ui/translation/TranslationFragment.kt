4a90e819d09c8ac08322acbd361e136bd7b1198a
GanievAR <ganievar@bnipi.rosneft.ru>
2022-06-07 12:49:49 +0500

    добавлен вывод ошибок

--- app/src/main/java/gunixun/dictionary/ui/translation/TranslationFragment.kt
+++ app/src/main/java/gunixun/dictionary/ui/translation/TranslationFragment.kt
@@ -5,9 +5,14 @@ import android.view.View
 import android.widget.SearchView
 import androidx.core.view.isVisible
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.material.snackbar.Snackbar
+import gunixun.dictionary.R
 import gunixun.dictionary.databinding.FragmentTranslationBinding
 import gunixun.dictionary.ui.BaseFragment
 import gunixun.dictionary.ui.utils.AppState
+import gunixun.dictionary.ui.utils.createErrSnackBar
+import gunixun.dictionary.ui.utils.createMsgSnackBar
+import gunixun.dictionary.ui.utils.hideSnackBar
 import org.koin.android.ext.android.inject
 
 class TranslationFragment :
@@ -17,6 +22,9 @@ class TranslationFragment :
 
     private lateinit var adapter: TranslationAdapter
     private val presenter: TranslationContract.TranslationPresenterInterface by inject()
+
+    private var retryIter: Int = 0
+    private var snackBar: Snackbar? = null
 
     companion object {
         fun newInstance() = TranslationFragment()
@@ -62,15 +70,32 @@ class TranslationFragment :
         binding.emptyTextView.isVisible = false
         when (appState) {
             is AppState.Loading -> {
+                hideSnackBar(snackBar)
                 binding.progressBar.isVisible= true
             }
             is AppState.Success -> {
+                retryIter = 0
                 if (appState.data.isEmpty()){
                     binding.emptyTextView.isVisible = true
                 }
                 adapter.setData(appState.data)
             }
-            is AppState.Error -> {}
+            is AppState.Error -> {
+                binding.emptyTextView.isVisible = true
+                if (retryIter < 3) {
+                    snackBar = binding.root.createErrSnackBar(
+                        text = appState.error.toString(),
+                        actionText = R.string.retry,
+                        { presenter.findWord(binding.wordSearchView.query.toString()) }
+                    )
+                    snackBar?.show()
+                } else {
+                    binding.root.createMsgSnackBar(
+                        text = this.resources.getString(R.string.fall_load_data)
+                    ).show()
+                }
+                retryIter++
+            }
         }
     }
 
